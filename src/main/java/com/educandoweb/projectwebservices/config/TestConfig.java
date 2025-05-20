a9925926a7b9a210c477be5d8c98ffce4169e978
Guilherme Floes <guilhermefloesmmarchi@gmail.com>
2025-05-20 13:47:25 -0300

    OrderItem

--- src/main/java/com/educandoweb/projectwebservices/config/TestConfig.java
+++ src/main/java/com/educandoweb/projectwebservices/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.educandoweb.projectwebservices.entities.Category;
 import com.educandoweb.projectwebservices.entities.Order;
+import com.educandoweb.projectwebservices.entities.OrderItem;
 import com.educandoweb.projectwebservices.entities.Product;
 import com.educandoweb.projectwebservices.entities.User;
 import com.educandoweb.projectwebservices.entities.enums.OrderStatus;
 import com.educandoweb.projectwebservices.repositories.CategoryRepository;
+import com.educandoweb.projectwebservices.repositories.OrderItemRepository;
 import com.educandoweb.projectwebservices.repositories.OrderRepository;
 import com.educandoweb.projectwebservices.repositories.ProductRepository;
 import com.educandoweb.projectwebservices.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -56,6 +61,13 @@ public class TestConfig implements CommandLineRunner {
 		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 		
 		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
+
+		
+		
 		userRepository.saveAll(Arrays.asList(u1,u2));
 		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
@@ -69,6 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		p5.getCategories().add(cat2);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+		
+		
 		
 	}
 	
--- src/main/java/com/educandoweb/projectwebservices/entities/Order.java
+++ src/main/java/com/educandoweb/projectwebservices/entities/Order.java
@@ -2,7 +2,9 @@ package com.educandoweb.projectwebservices.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.educandoweb.projectwebservices.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 
@@ -37,6 +40,9 @@ public class Order implements Serializable{
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 	
 	public Order() {
 		
@@ -91,6 +97,9 @@ public class Order implements Serializable{
 		}
 	}
 
+	public Set<OrderItem> getItems(){
+		return items;
+	}
 
 	@Override
 	public int hashCode() {
