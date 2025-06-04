833b075c5cc16862d904c8de79b4999f8142490e
Guilherme Floes <guilhermefloesmmarchi@gmail.com>
2025-06-04 15:18:00 -0300

    Exceptions

--- src/main/java/com/educandoweb/projectwebservices/services/UserService.java
+++ src/main/java/com/educandoweb/projectwebservices/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.projectwebservices.entities.User;
 import com.educandoweb.projectwebservices.repositories.UserRepository;
+import com.educandoweb.projectwebservices.services.exceptions.DataBaseException;
 import com.educandoweb.projectwebservices.services.exceptions.ResourceNotFoundException;
 
 
@@ -32,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
+		}catch(EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch(DataIntegrityViolationException e) {
+			throw new DataBaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
